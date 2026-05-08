6b9158962c1882eae134ef287880be620f51d0b9
Lennard Zehner <Moeweneier@users.noreply.github.com>
2026-05-08 22:16:17 +0200

    Polish Imposter game

---
+++ questions.js
@@ -0,0 +1,10 @@
+window.IMPOSTER_QUESTIONS = [
+    { normal: "Pizza", imposter: "Burger" },
+    { normal: "Paris", imposter: "London" },
+    { normal: "Hund", imposter: "Katze" },
+    { normal: "Sommer", imposter: "Winter" },
+    { normal: "Ferrari", imposter: "Lamborghini" },
+    { normal: "Strandurlaub", imposter: "Skiurlaub" },
+    { normal: "Netflix", imposter: "YouTube" },
+    { normal: "Kaffee", imposter: "Energy Drink" }
+];
